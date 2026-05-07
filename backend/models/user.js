384a3e04b0484296ee476adf477a4d4fe7abcf7c
aysegul <aysegulbuluts53@gmail.com>
2026-05-07 16:54:21 +0300

    fix: remove unnecessary root package files

---
+++ backend/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "İsim zorunludur"],
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email zorunludur"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Şifre zorunludur"],
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
